9f8d984af796278f5a7a362e3286f76550a527e5
devdiegomt <devdiegomt@gmail.com>
2026-08-11 13:18:58 -0500

    fix: api fixed.

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,59 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export async function getCourses() {
+
+  const response =
+    await fetch(`${BASE_URL}/courses`);
+
+  return response.json();
+}
+
+export async function getTopics(courseId) {
+
+  const response =
+    await fetch(
+      `${BASE_URL}/courses/${courseId}/topics`
+    );
+
+  return response.json();
+}
+
+export async function getCoursework(
+  courseId,
+  topicId
+) {
+
+  const response =
+    await fetch(
+      `${BASE_URL}/courses/${courseId}/topics/${topicId}/coursework`
+    );
+
+  return response.json();
+}
+
+export async function getSubmissions(
+  courseId,
+  courseWorkId
+) {
+
+  const response =
+    await fetch(
+      `${BASE_URL}/courses/${courseId}/coursework/${courseWorkId}/submissions`
+    );
+
+  return response.json();
+}
+
+export async function getStudents(courseId) {
+
+  const response =
+    await fetch(
+      `${BASE_URL}/courses/${courseId}/students`
+    );
+
+  return response.json();
+}
+
 export async function createTopic(courseId, name) {
 
   const response =
